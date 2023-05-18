ad0d67264c2183c5cf64630e9281dcbd2c4ad2f9
Nighfire3250 <ernestoraf2011@gmail.com>
2023-05-17 23:47:51 -0600

    enrutamiento y prueba datagrid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Login from '../src/views/LoginPage/Login';
+import Datagrid from "./views/DatagridPage/Datagrid";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+const App = () => {
+  return(
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Datagrid" element={<Datagrid />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/views/DatagridPage/Datagrid.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+
+const Datagrid = () => {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      // Datos del Servidor
+      axios.get('http://localhost:4000/home')
+        .then(response => {
+          setData(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+  
+    // Columnas de DataGrid
+    const columns = [
+      { field: 'idUsuario', headerName: 'ID', width: 100 },
+      { field: 'correo', headerName: 'Correo', width: 200 },
+      { field: 'userStatus', headerName: 'Estado', width: 200 },
+    ];
+  
+    return (
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid rows={data} columns={columns} getRowId={(row) => row.idUsuario} />
+      </div>
+    );
+  };
+  
+  export default Datagrid;
